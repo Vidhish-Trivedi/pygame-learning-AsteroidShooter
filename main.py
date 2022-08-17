866ea5247cd345149bc5e21cf52ace14f18519a9
Vidhish Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2022-08-17 17:23:28 +0530

    Time and timers.
    Learnt how to check time since start of game and create timers.

--- main.py
+++ main.py
@@ -2,17 +2,30 @@ from math import ceil
 import pygame as pg
 import sys
 
-#####################################  CONSTANTS AND FUNCTIONS #####################################
+#####################################  VARIABLES AND CONSTANTS AND FUNCTIONS #####################################
 pg.init()  # Initialise pygame.
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+tf = 0
+t_r = None
+last_fire_time = -1
 
-def lsr_move(lsr_list, speed = 120):
+def lsr_move(lsr_list, speed = 200):
     for lsr in lsr_list:
         lsr.centery -= ceil(speed*dt)
         # Optimization. (Remove lasers which are outside the screen).
         if(lsr.bottom < 0):
             lasers.remove(lsr)
+
+def get_time_since_start(tf, t_r):
+    if(tf != 0):
+        pg.draw.rect(bg_surf, color=(42,45,51), rect=t_r, width=8, border_radius=5)
+
+    time_txt = f"Score: {pg.time.get_ticks()//1000}"
+    txt_surf = font1.render(time_txt, True, (255, 255, 255))  # AntiAlias (bool) : smooth out edges of font.
+    txt_rect = txt_surf.get_rect(midbottom=(WINDOW_WIDTH/2, WINDOW_HEIGHT - 30))
+    display_surface.blit(txt_surf, txt_rect)
+    return(pg.draw.rect(bg_surf, color='white', rect=txt_rect.inflate(50, 50), width=8, border_radius=5))
 
 #####################################  CREATE A WINDOW  #################################
 # Set up a window.
@@ -32,28 +45,22 @@ laser_surf = pg.image.load('./graphics/laser.png').convert_alpha()
 # laser_rect = laser_surf.get_rect(midbottom=ship_rect.center)
 
 # Creating text.
-font1 = pg.font.Font('./graphics/subatomic.ttf', 50)
-txt_surf = font1.render("Space", True, (255, 255, 255))  # AntiAlias (bool) : smooth out edges of font.
-txt_rect = txt_surf.get_rect(midbottom=(WINDOW_WIDTH/2, WINDOW_HEIGHT - 25))
+font1 = pg.font.Font('./graphics/subatomic.ttf', 40)
+# txt_surf = font1.render(f"Score: {pg.time.get_ticks()}", True, (255, 255, 255))  # AntiAlias (bool) : smooth out edges of font.
+# txt_rect = txt_surf.get_rect(midbottom=(WINDOW_WIDTH/2, WINDOW_HEIGHT - 25))
 
+##############################################  TIME IN PYGAME  ##############################################
+# Pygame can get the time since pygame.init() was called.
+# We can create repeated timers.
+
+##############################  CREATING A TIMER  ################################
+# Check start time and end time (current time), their difference can be used.
+
+##############################################################################################################
 # Set up a clock.
 clk = pg.time.Clock()   # But what if the game runs even slower?.
 
 # Ideally, we don't want to limit the frame rate, so that the game is 'fluid'.
-
-#############################################################  DELTA TIME  ######################################################################
-# Measures how long it took to create our current frame (one frame).
-# For example, if frame rate is 60 fps, then DT = 1/60 = 0.0167 seconds.  (This can vary depending on frame rate).
-# This information can be used to keep the game at a constant speed regardless of framerate.
-# We multiply any movement inside the game with delta time.  (speed)*(frame rate)*(delta time) ==> (pixels per second)*(FPS)*(seconds per frame).
-# Now, if the frame rate becomes half, DT becomes twice, negating any effect on time taken for movement.
-
-######################  PROBLEMS WITH DELTA TIME  ############################
-# When moving rects, we always place integers (at pixel positions (can not be decimals)).
-# But, DT gives us floating point values, which are converted by pygame.
-# For example, 5(original pos) + 0.835(move by) = 5.835 ==> 5 (by pygame) (for ever at 5 (recursion)). # HERE, NO MOVEMENT OCCURS.
-# So, movement can sporadically stop/fail.
-#################################################################################################################################################
 
 while(True):
     # 1.) Inputs (event loop).
@@ -64,9 +71,12 @@ while(True):
             sys.exit()
 
         if(event.type == pg.MOUSEBUTTONDOWN):
-            laser_rect = laser_surf.get_rect(midbottom=ship_rect.center)
-            lasers.append(laser_rect)
-            # print(len(lasers), lasers)
+            # Applying timer logic to limit how fast (once every half second) lasers can be shot,
+            # else, game would become too easy.
+            if(pg.time.get_ticks() - last_fire_time >= 500):
+                laser_rect = laser_surf.get_rect(midbottom=ship_rect.center)
+                lasers.append(laser_rect)
+                last_fire_time = pg.time.get_ticks()
 
     # Get delta time.
     dt = clk.tick(120)/1000  # in milliseconds.
@@ -75,17 +85,22 @@ while(True):
     ship_rect.center = pg.mouse.get_pos()
     
     # Applying delta time concept for movement.
-    # laser_rect.centery -= ceil(1*dt)  # Move laser upwards. (By integer, round up to avoid problem with DT (mentioned above) ==> game becomes faster than it is supposed to be).
-    # laser_rect.centery -= ceil(120*dt)
     lsr_move(lasers)
 
+    # Get time since game started (since pg.init() was called).
+    # time_since_start = pg.time.get_ticks() # milliseconds in seconds.
+
     # 2.) Update elements.
+    display_surface.fill("black")
     display_surface.blit(bg_surf, (0, 0))
     
-    display_surface.blit(txt_surf, txt_rect)
-    pg.draw.rect(bg_surf, color='white', rect=txt_rect.inflate(30, 30), width=8, border_radius=5)
+    # display_surface.blit(txt_surf, txt_rect)
+    # pg.draw.rect(bg_surf, color='white', rect=txt_rect.inflate(30, 30), width=8, border_radius=5)
     
-    # display_surface.blit(laser_surf, laser_rect)
+    # Using some temporary variables to display updated values such as time or score.
+    t_r = get_time_since_start(tf, t_r)
+    tf += 1
+    
     # Display lasers.
     for lsr in lasers:
         display_surface.blit(laser_surf, lsr)
